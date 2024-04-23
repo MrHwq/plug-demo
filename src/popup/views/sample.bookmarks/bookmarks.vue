2a342aef9c802c115591a2270811462b04a64d6a
hwqgooo <mrhwq@foxmail.com>
2024-04-23 10:30:54 +0800

    feature: 展示书签界面

--- src/popup/views/sample.bookmarks/bookmarks.vue
+++ src/popup/views/sample.bookmarks/bookmarks.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
+  <div v-loading="loading">
     <el-input v-model="search" placeholder="请输入内容" clearable>
       <template slot="append">
         <el-button @click="dumpBookmarks(search)">搜索</el-button>
       </template>"
     </el-input>
-    <div>{{ bookmarkNodes }}</div>
+    <!-- <div>{{ bookmarkNodes }}</div> -->
+    <el-tree :data="bookmarkNodes" :props="defaultProps">
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span style="padding-left: 30px;">
+          <el-button type="text" size="mini" @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button type="text" size="mini" @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
   </div>
 </template>
 
 <script>
 export default {
-  data() { return { bookmarkNodes: {}, seeach: '' } },
+  data() {
+    return {
+      bookmarkNodes: [],
+      search: '',
+      defaultProps: {
+        children: 'children',
+        label: 'title'
+      },
+      loading: false
+    }
+  },
   created() { },
   mounted() {
     this.dumpBookmarks();
@@ -21,10 +44,16 @@ export default {
     },
     dumpBookmarks(query) {
       console.log(query)
+      this.loading = true
       this.bookmarkNodes.query = query
       chrome.bookmarks.getTree((bookmarkNodes) => {
         console.log(bookmarkNodes);
+        if (!(bookmarkNodes[0] || { title: 'none' }).title) {
+          bookmarkNodes[0].title = '书签栏'
+        }
+        console.log(bookmarkNodes)
         this.bookmarkNodes = bookmarkNodes;
+        this.loading = false
       });
     }
   }
